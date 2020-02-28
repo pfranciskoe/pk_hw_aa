d4bc985d02086d8176fb5c79cf711a81e066293a
peter <pfranciskoe@gmail.com>
2020-02-27 21:16:24 -0800

    finished w3d4 hw

--- W3D4/DIY_ADT/map.rb
+++ W3D4/DIY_ADT/map.rb
@@ -1,3 +1,37 @@
 class Map
+    def initialize
+    @map_arr = []
+    end
+    def set(key,value)
+        if @map_arr.none?{|ele| ele[0] == key}
+            @map_arr << [key,value]
+        else
+            @map_arr.map do |ele|
+                if ele[0] == key
+                [key,value]
+                else
+                ele
+                end
+            end
+        end
+    end
+
+    def get(key)
+        @map_arr.each {|ele| return ele if ele[0] == key}
+    end
+
+    def delete(key)
+        @map_arr.each.with_index do |ele,idx|
+            if ele[0] == key
+                @map_arr.delete_at(idx)
+                return
+            end
+        end
+    end
+    
+    def show
+        @map_arr
+    end
+
 
 end
