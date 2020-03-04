2242c7619d150376d7cca9e555583ae16eab8e53
peter <pfranciskoe@gmail.com>
2020-03-03 20:38:00 -0800

    added stuff

--- W4D2/pk_mancala/lib/board.rb
+++ W4D2/pk_mancala/lib/board.rb
@@ -22,26 +22,62 @@ class Board
     raise "Starting cup is empty" if @cups[start_pos].empty?
   end
 
-  def make_move(start_pos, current_player_name)
-    stones_to_be_moved = @cups[start_pos].dup
-    @cups[start_pos] = []
-    i = start_pos
-    if current_player_name == @name1
-      enemy_home = 13
-    else
-      enemy_home = 6
-    end
+  # def make_move(start_pos, current_player_name)
+  #   stones_to_be_moved = @cups[start_pos].dup
+  #   @cups[start_pos] = []
+  #   i = start_pos
 
-   while !stones_to_be_moved.empty?
-      if i == 6
-        @cups[i] << stones_to_be_moved.shift
-        i+=1
+  #   if current_player_name == @name1
+  #     enemy_home = 13
+  #   else
+  #     enemy_home = 6
+  #   end
+
+  #  while !stones_to_be_moved.empty?
+  #     if i != enemy_home
+  #       @cups[i] << stones_to_be_moved.pop
+  #       i += 1
+  #       i = 0 if i > 13
+  #     end
+  #   end
+  #   next_turn(i)
+  # end
+  def make_move(start_pos, current_player_name)
+    # empties cup
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    # distributes stones
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      # places stones in the correct current player's cups
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
       end
     end
+
+    render
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    # helper method to determine what #make_move returns
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      # ended on store -- get to choose where to start again
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      # ended on empty cup -- switches players' turns
+      :switch
+    else
+      # ended on cup with stones in it -- automatically starts there
+      ending_cup_idx
+    end
   end
 
   def render
