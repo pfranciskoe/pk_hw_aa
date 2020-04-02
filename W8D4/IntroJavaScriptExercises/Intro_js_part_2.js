873ab4583f82f5d1be83930aa5efaf21dae4a2f8
peter <pfranciskoe@gmail.com>
2020-04-02 09:01:01 -0700

    uploaded elephant contructor

--- W8D4/IntroJavaScriptExercises/Intro_js_part_2.js
+++ W8D4/IntroJavaScriptExercises/Intro_js_part_2.js
@@ -1,4 +1,23 @@
 titleize = function(arr,callback) {
-    let new_words = [];
-    
-}
+    let new_words = arr.map(word => `Mx. ${word} Jingleheimer Schmidt`);
+    callback(new_words);
+};
+
+function Elephant(name,height,tricks) {
+    this.name = name;
+    this.height = height;
+    this.tricks = tricks;
+};
+
+Elephant.prototype.trumpet = function() {
+    console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`)
+};
+
+Elephant.prototype.grow = function() {
+    this.height++;
+};
+
+Elephant.prototype.addTrick = function(trick) {
+    this.tricks.push(trick)
+};
+
